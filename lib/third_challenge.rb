9b93a0afc358384ed58819722077a7b6144f5002
derickcastillo38 <9derick@gmail.com>
2018-06-22 04:22:51 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,7 +2,7 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        name: {}, age: {}
+        name: {"Lord Montague" => "53"}, age: {"Lord Montague" => "53"}
         #your key/value pairs here
         },
       :matriarch => {
